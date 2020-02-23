b567484edd9a819033a17eac2b45c48fd2017da9
Megan Comeau <mlcomeau99@gmail.com>
2020-02-23 23:32:26 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,5 +28,17 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-
+  switch (age) {
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+      console.log("You are a teenager");
+      break;
+    default:
+      console.log("You have an age");  
+  }
 }
